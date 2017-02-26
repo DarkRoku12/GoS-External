624000b398ec8ba1398d0e468d2c851940d0f2e4
Toshibiotro <ademdark@live.co.uk>
2017-02-26 17:31:00 +0000

    Update ImHavingFunSeries.lua

--- ImHavingFunSeries.lua
+++ ImHavingFunSeries.lua
@@ -4,6 +4,8 @@ function Common:__init()
 	
 	require("MapPosition")
 	
+	self.str = {[0] = "Q", [1] = "W", [2] = "E", [3] = "R"}
+	
 	self.ImmobileBuffs = {
 		[5] = true,
 		[11] = true,
@@ -112,7 +114,7 @@ end
 function Common:GetPrediction(unit, spellData)
 	local pos = unit.posTo
 	local unitpos = unit.pos
-	if pos == unitpos then
+	if pos == unitpos or self:IsImmobile(unit) then
 		return unitpos
 	end
 	local speed = spellData.speed
@@ -488,12 +490,11 @@ function Ezreal:__init()
 	
 	self.Menu:MenuElement({id = "Draw", name = "Drawings", type = MENU})
 	self.Menu.Draw:MenuElement({id = "DA", name = "Disable All Drawings", value = false})
-	self.str = {[0] = "Q", [1] = "W", [2] = "E", [3] = "R"}
 	for i = 0, 2 do
-		self.Menu.Draw:MenuElement({id = "D"..self.str[i], name = "Draw "..self.str[i].." Range", type = MENU})
-		self.Menu.Draw["D"..self.str[i]]:MenuElement({id = "Enabled", name = "Enabled", value = true})
-		self.Menu.Draw["D"..self.str[i]]:MenuElement({id = "Width", name = "Width", value = 2, min = 1, max = 5, step = 1})
-		self.Menu.Draw["D"..self.str[i]]:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 255, 255, 255)})
+		self.Menu.Draw:MenuElement({id = "D"..Common.str[i], name = "Draw "..Common.str[i].." Range", type = MENU})
+		self.Menu.Draw["D"..Common.str[i]]:MenuElement({id = "Enabled", name = "Enabled", value = true})
+		self.Menu.Draw["D"..Common.str[i]]:MenuElement({id = "Width", name = "Width", value = 2, min = 1, max = 5, step = 1})
+		self.Menu.Draw["D"..Common.str[i]]:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 255, 255, 255)})
 	end
 	
 	self.Menu:MenuElement({id = "Misc", name = "Misc", type = MENU})
@@ -523,7 +524,7 @@ function Ezreal:Draw()
 		return
 	end
 	for i = 0, 2 do
-		local Str = self.str[i]
+		local Str = Common.str[i]
 		if self.Menu.Draw["D"..Str].Enabled:Value() then
 			Draw.Circle(myHero.pos, self.Spells[i].range, self.Menu.Draw["D"..Str].Width:Value(), self.Menu.Draw["D"..Str].Color:Value())
 		end
@@ -617,6 +618,216 @@ function Ezreal:CastR(unit)
 		Pred = myHero.pos + (Pred - myHero.pos):Normalized() * 500
 		Order:CastSpell(_R, Pred)
 	end
+end
+
+class "Lux"
+
+function Lux:__init()
+
+	self.Spells = {
+		[0] = {delay = 0.25, speed = 1600, range = 1300, width = 70},
+		[1] = {delay = 0.25, speed = 1400, range = 1075},
+		[2] = {delay = 0.25, speed = 1300, range = 1100, width = 350},
+		[3] = {delay = 1, speed = math.huge, range = 3500, width = 190}
+	}
+	
+	self.Damage = {
+		[0] = function(unit)
+			return EOW:CalcMagicalDamage(myHero, unit, (50 * myHero:GetSpellData(_Q).level) + (myHero.ap * 0.7))
+		end,
+		[2] = function(unit)
+			return EOW:CalcMagicalDamage(myHero, unit, 15 + (45 * myHero:GetSpellData(_E).level) + (myHero.ap * 0.6))
+		end,
+		[3] = function(unit)
+			local BaseDamage = 200 + (100 * myHero:GetSpellData(_R).level) + (myHero.ap * 0.75)
+			local BonusDamage = Common:GotBuff(unit, "luxilluminatingfraulein") > 0 and 10 + (myHero.levelData.lvl * 10) + (myHero.ap * 0.2) or 0
+			return EOW:CalcMagicalDamage(myHero, unit, BaseDamage + BonusDamage)
+		end
+	}
+	
+	self.Target = nil
+	
+	self.Menu = MenuElement({id = "Lux", name = "Lux", type = MENU, leftIcon = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/"..myHero.charName..".png"})
+	
+	self.Menu:MenuElement({id = "Combo", name = "Combo", type = MENU})
+	self.Menu.Combo:MenuElement({id = "CQ", name = "Use Q", value = true})
+	self.Menu.Combo:MenuElement({id = "CE", name = "Use E", value = true})
+	self.Menu.Combo:MenuElement({id = "CR", name = "Use R", value = true})
+	
+	self.Menu:MenuElement({id = "Harass", name = "Harass", type = MENU})
+	self.Menu.Harass:MenuElement({id = "HQ", name = "Use Q", value = true})
+	self.Menu.Harass:MenuElement({id = "HE", name = "Use E", value = true})
+	self.Menu.Harass:MenuElement({id = "HMM", name = "Min Mana To Harass", value = 60, min = 1, max = 100, step = 1})
+	
+	self.Menu:MenuElement({id = "Killsteal", name = "Killsteal", type = MENU})
+	self.Menu.Killsteal:MenuElement({id = "Enabled", name = "Enabled", value = true})
+	self.Menu.Killsteal:MenuElement({id = "KSQ", name = "Use Q", value = true})
+	self.Menu.Killsteal:MenuElement({id = "KSE", name = "Use E", value = true})
+	self.Menu.Killsteal:MenuElement({id = "KSR", name = "Use R", value = true})
+	
+	self.Menu:MenuElement({id = "Misc", name = "Misc", type = MENU})
+	self.Menu.Misc:MenuElement({id = "AE", name = "Auto E2", value = true})
+	self.Menu.Misc:MenuElement({id = "AQCC", name = "Auto Q On CC", value = true})
+	self.Menu.Misc:MenuElement({id = "AECC", name = "Auto E On CC", value = true})
+	
+	self.Menu:MenuElement({id = "Draw", name = "Drawings", type = MENU})
+	self.Menu.Draw:MenuElement({id = "DA", name = "Disable All Draws", value = true})
+	for i = 0, 3 do
+		local k = Common.str[i]
+		self.Menu.Draw:MenuElement({id = "D"..k, name = "Draw "..k.." Range", type = MENU})
+		self.Menu.Draw["D"..k]:MenuElement({id = "Enabled", name = "Enabled", value = true})
+		self.Menu.Draw["D"..k]:MenuElement({id = "Width", name = "Width", value = 2, min = 1, max = 5, step = 1})
+		self.Menu.Draw["D"..k]:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 255, 255, 255)})
+	end
+	
+	Callback.Add("Tick", function() self:Tick() end)
+	Callback.Add("Draw", function() self:Draw() end)
+end
+
+function Lux:Tick()
+	self.Target = EOW:GetTarget() or Common:GetTarget()
+	
+	if EOW:Mode() == "Combo" then
+		self:Combo()
+	elseif EOW:Mode() == "Harass" then
+		self:Harass()
+	end
+	
+	self:Auto()
+	
+	if self.Menu.Killsteal.Enabled:Value() then
+		self:Killsteal()
+	end
+end
+
+function Lux:Draw()
+	if self.Menu.Draw.DA:Value() then
+		return
+	end
+	for i = 0, 3 do
+		local k = Common.str[i]
+		if self.Menu.Draw["D"..k].Enabled:Value() then
+			Draw.Circle(myHero.pos, self.Spells[i].range, self.Menu.Draw["D"..k].Width:Value(), self.Menu.Draw["D"..k].Color:Value())
+		end
+	end
+end
+
+function Lux:Combo()
+	if myHero.attackData.state == STATE_WINDUP then
+		return
+	end
+	
+	if self.Menu.Combo.CQ:Value() and Common:Ready(_Q) and Common:ValidTarget(self.Target, self.Spells[0].range) then
+		self:CastQ(self.Target)
+	end
+	
+	if self.Menu.Combo.CE:Value() and Common:Ready(_E) then
+		if not self:IsE2() and Common:ValidTarget(self.Target, self.Spells[2].range) then
+			self:CastE(self.Target)
+		elseif self:IsE2() then
+			Order:CastSpell(_E)
+		end
+	end
+	
+	if self.Menu.Combo.CR:Value() and Common:Ready(_R) and Common:ValidTarget(self.Target, self.Spells[3].range) and self.Damage[3](self.Target) >= self.Target.health + self.Target.shieldAP then
+		self:CastR(self.Target)
+	end
+end
+
+function Lux:Harass()
+	if myHero.attackData.state == STATE_WINDUP or Common:GetPercentMP(myHero) < self.Menu.Harass.HMM:Value() then
+		return
+	end
+	
+	if self.Menu.Harass.HQ:Value() and Common:Ready(_Q) and Common:ValidTarget(self.Target, self.Spells[0].range) then
+		self:CastQ(self.Target)
+	end
+	
+	if self.Menu.Harass.HE:Value() and Common:Ready(_E) then
+		if not self:IsE2() and Common:ValidTarget(self.Target, self.Spells[2].range) then
+			self:CastE(self.Target)
+		elseif self:IsE2() then
+			Order:CastSpell(_E)
+		end
+	end
+end
+
+function Lux:Killsteal()
+	if myHero.attackData.state == STATE_WINDUP then
+		return
+	end
+	for i = 1, Game.HeroCount() do
+		local h = Game.Hero(i)
+		if h and h.isEnemy then
+			if self.Menu.Killsteal.KSQ:Value() and Common:Ready(_Q) and Common:ValidTarget(h, self.Spells[0].range) and self.Damage[0](h) >= h.health + h.shieldAP then
+				self:CastQ(h)
+				break
+			elseif self.Menu.Killsteal.KSE:Value() and Common:Ready(_E) and self.Damage[2](h) >= h.health + h.shieldAP then
+				if not self:IsE2() and Common:ValidTarget(h, self.Spells[2].range) then
+					self:CastE(h)
+					break
+				elseif self:IsE2() then
+					Order:CastSpell(_E)
+					break
+				end
+			elseif self.Menu.Killsteal.KSR:Value() and Common:Ready(_R) and Common:ValidTarget(h, self.Spells[3].range) and self.Damage[3](h) >= h.health + h.shieldAP then
+				self:CastR(h)
+				break
+			end
+		end
+	end
+end
+
+function Lux:Auto()
+	if self.Menu.Misc.AE:Value() and self:IsE2() then
+		Order:CastSpell(_E)
+	end
+	for i = 1, Game.HeroCount() do
+		local h = Game.Hero(i)
+		if h and h.isEnemy then 
+			if self.Menu.Misc.AECC:Value() and Common:Ready(_E) and Common:ValidTarget(h, self.Spells[2].range) and Common:IsImmobile(h) then
+				self:CastE(h)
+			elseif self.Menu.Misc.AQCC:Value() and Common:Ready(_Q) and not h.dead and Common:GetDistance(myHero.pos, h.pos) <= self.Spells[0].range then
+				local ExpireTime = Common:IsImmobile(h)
+				local TimeToReach = self.Spells[0].delay + (Common:GetDistance(myHero.pos, h.pos) / self.Spells[0].speed)
+				if ExpireTime and ExpireTime > Game.Timer() + TimeToReach and Game.Timer() < ExpireTime - TimeToReach then
+					self:CastQ(h)
+				end
+			end
+		end	
+	end
+end
+
+function Lux:CastQ(unit)
+	local Pred = Common:GetPrediction(unit, self.Spells[0])
+	if Pred and Common:GetHeroCollision(myHero.pos, Pred, self.Spells[0].width, unit) + Common:GetMinionCollision(myHero.pos, Pred, self.Spells[0].width, unit) < 2 then
+		Pred = Vector(Pred)
+		Pred = myHero.pos + (Pred - myHero.pos):Normalized() * 300
+		Order:CastSpell(_Q, Pred)
+	end
+end
+
+function Lux:CastE(unit)
+	if self:IsE2() then
+		return
+	end
+	local Pred = Common:GetPrediction(unit, self.Spells[2])
+	if Pred then
+		Order:CastSpell(_E, Pred)
+	end
+end
+
+function Lux:CastR(unit)
+	local Pred = Common:GetPrediction(unit, self.Spells[3])
+	if Pred then
+		Pred = Vector(Pred)
+		Pred = myHero.pos + (Pred - myHero.pos):Normalized() * 300
+		Order:CastSpell(_R, Pred)
+	end
+end
+
+function Lux:IsE2()
+	return myHero:GetSpellData(_E).toggleState == 2
 end
 
 Callback.Add("Load", function()
